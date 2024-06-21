a4ae1ef1beebb1a8090536705ac78118f0de34f5
jones-2earth <165614037+jones-2earth@users.noreply.github.com>
2024-06-21 14:20:13 +0900

    ユーザー管理機能修正点１

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,18 @@ class User < ApplicationRecord
   has_many :orders
 
   validates :nickname, presence: true
+  validates :email, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birthday, presence: true
 
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'}
+  
+  with_options presence: true do
+    validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: 'is invalid. Input full-width characters' }
+    validates :last_name_kana,:first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters' }
+  end
 end
