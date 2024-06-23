24d6ed6de18f89f98a4bcc5ad17220c50740daac
jones-2earth <165614037+jones-2earth@users.noreply.github.com>
2024-06-23 10:24:00 +0900

    配送料の負担のハッシュ

--- spec/models/shipping_cost_spec.rb
+++ spec/models/shipping_cost_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe ConditionSkipMigration, type: :model do
+RSpec.describe ShippingCost, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
