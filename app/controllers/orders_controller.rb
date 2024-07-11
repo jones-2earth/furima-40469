f27cdf79fdaadd2b3c54a9fbbdb6c904324413dc
jones-2earth <165614037+jones-2earth@users.noreply.github.com>
2024-07-11 18:05:01 +0900

    商品一覧表示機能修正依頼箇所④

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,24 +1,24 @@
-class OrdersController < ApplicationController
-  before_action :authenticate_user!
-  before_action :set_item, only: [:index, :create]
+#class OrdersController < ApplicationController
+  #before_action :authenticate_user!
+  #before_action :set_item, only: [:index, :create]
 
-  def index
-    if user_signed_in? && current_user.id != @item.user_id && @item.order == nil
-      @item_order = ItemOrder.new
-    else
-      redirect_to root_path
-    end
-  end
+  #def index
+    #if user_signed_in? && current_user.id != @item.user_id && @item.order == nil
+      #@item_order = ItemOrder.new
+    #else
+      #redirect_to root_path
+    #end
+  #end
 
 
-  private
+  #private
 
-  def set_item
-    @item = Item.find(params[:item_id])
-  end
+  #def set_item
+    #@item = Item.find(params[:item_id])
+  #end
 
   #def order_params
     #params.require(:order).permit(:content, :image).merge(user_id: current_user.id)
   #end
 
-end
+#end
