51a4c0f92f4f59a59b7c71d3ffcd64d914267d90
jones-2earth <165614037+jones-2earth@users.noreply.github.com>
2024-07-18 15:17:55 +0900

    商品削除機能

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,7 @@
-import { Application } from "@hotwired/stimulus"
-
-const application = Application.start()
-
-// Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
 import "item_price"
-
-export { application }
-
+import "@hotwired/turbo-rails"
+import "controllers"
+import "item_price"
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items do
-    resources :orders, only: [:index, :create, :show, :edit, :update]
-  end
+  resources :items
     
   
 end
