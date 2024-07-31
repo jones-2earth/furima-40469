f38caaa924d8a17f23eb34d6c5325f05ae3989ba
jones-2earth <165614037+jones-2earth@users.noreply.github.com>
2024-07-31 17:20:40 +0900

    デプロイできないため②

--- db/schema.rb
+++ db/schema.rb
@@ -96,6 +96,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_26_083815) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
