0b1581c74074c399866c00080345805587e90c76
jones-2earth <165614037+jones-2earth@users.noreply.github.com>
2024-08-01 14:57:49 +0900

    最終課題提出後修正依頼②

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,13 +1,13 @@
-
-window.addEventListener('turbo:load', () => {
+const price = () => {
   const priceInput = document.getElementById("item-price");
-  priceInput.addEventListener("input", () => {
+  priceInput.addEventListener("input", function(){
   const inputValue = priceInput.value;
   const addTaxDom = document.getElementById("add-tax-price");
-  const ProfitDom = document.getElementById("profit");
-  // 入力した金額をもとに販売手数料を計算する処理
   addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
-  // 出品価格から販売手数料を引く処理
-  ProfitDom.innerHTML = inputValue - addTaxDom.innerHTML;
-  })
-});
+  const profitNumber = document.getElementById("profit");
+  profitNumber.innerHTML = Math.floor(inputValue - addTaxDom.innerHTML);
+  });
+};
+
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
--- app/models/item.rb
+++ app/models/item.rb
@@ -27,11 +27,9 @@ class Item < ApplicationRecord
     validates :shipping_cost_id
     validates :prefecture_id
     validates :shipping_date_id
-  # 300円以上かつ9,999,999円以下で、半角数字でないと入力不可
     validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 
-  # ジャンルの選択が「--」の時は保存不可
   with_options numericality: { other_than: 0 } do
     validates :category_id
     validates :prefecture_id
