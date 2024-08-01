6ab9d4f85931dd4b2a5ce915473fc114dd8e49a7
jones-2earth <165614037+jones-2earth@users.noreply.github.com>
2024-08-01 17:12:24 +0900

    最終課題提出後修正依頼④

--- db/migrate/20240614021320_create_areas.rb
+++
@@ -1,8 +0,0 @@
-class CreateAreas < ActiveRecord::Migration[7.0]
-  def change
-    create_table :areas do |t|
-      t.integer     :prefecture_id,     null:false
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -39,12 +39,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_26_083815) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "areas", charset: "utf8", force: :cascade do |t|
-    t.integer "prefecture_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "items", charset: "utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "name", null: false
